36d247ac9de26cf19a386e555174ebba666ed81d
David <davidaase@hotmail.com>
2022-03-09 15:52:15 +0100

    init example project

---
+++ src/main/kotlin/org/example/Hello.kt
@@ -0,0 +1,12 @@
+package org.example
+
+import io.javalin.Javalin
+
+fun main() {
+    val app = Javalin.create().start(7070)
+
+    app.get("/") { ctx ->
+        ctx.result("Hello World")
+    }
+
+}
